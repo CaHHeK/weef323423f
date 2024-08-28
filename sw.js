585f668cf5ece3f44341664326c409658201333f
CaHHeK <90181409+CaHHeK@users.noreply.github.com>
2024-08-28 20:57:51 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,18 @@
 self.addEventListener('push', event => {
     const data = event.data.json();
     self.registration.showNotification('Забери код', {
-        body: `Ваш код: ${data.code}`,
+        body: 'Нажмите, чтобы получить ваш код',
         icon: 'https://cahhek.github.io/weef323423f/unnamed.jpg',
         data: {
-            code: data.code,
-            url: 'https://t.me/hamsTer_kombat_bot/start'
+            url: 'https://cahhek.github.io/weef323423f/codes.html'
         }
     });
 });
 
 self.addEventListener('notificationclick', event => {
-    const { code, url } = event.notification.data;
-
     event.notification.close();
 
     event.waitUntil(
-        clients.openWindow(url).then(() => {
-            // Копируем код в буфер обмена после открытия URL
-            return fetch('/copy-code', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ code })
-            });
-        })
+        clients.openWindow(event.notification.data.url)
     );
 });
